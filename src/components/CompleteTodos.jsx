ed2a8cbbd3abd70264ed1ffcda1ba47a5554da09
purazuma0427 <118142881+purazuma0427@users.noreply.github.com>
2023-04-11 18:38:06 +0900

    Initial commit

---
+++ src/components/CompleteTodos.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const CompleteTodos = (props) => {
+  const { completeTodos, onClickBack, disabled } = props;
+  return (
+    <div className="complete-area">
+      <p className="title">完了のTODO</p>
+      <ul>
+        {completeTodos.map((todo, index) => {
+          return (
+            <div key={todo} className="list-row">
+              <li>{todo}</li>
+              <button disabled={disabled} onClick={() => onClickBack(index)}>
+                戻す
+              </button>
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
